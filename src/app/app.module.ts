238b0f32e6d16b6c27afbb8860d9444c007eb55f
Ahsan Ayaz <ahsan.ubitian@gmail.com>
2018-05-30 15:53:31 +0500

    added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
+import { NotesHeaderComponent } from './components/notes-header/notes-header.component';
+import { NotesEditorComponent } from './components/notes-editor/notes-editor.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    NotesListComponent,
+    NotesHeaderComponent,
+    NotesEditorComponent
   ],
   imports: [
     BrowserModule,
